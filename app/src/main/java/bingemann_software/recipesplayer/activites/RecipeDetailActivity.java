5699b50c7e1525d43d1beac23e7c8b56a4988890
JanNB97 <j.bingemann@gmx.de>
2018-10-16 16:05:26 +0200

    detail activity accessable over add button

--- app/src/main/java/bingemann_software/recipesplayer/activites/RecipeDetailActivity.java
+++ app/src/main/java/bingemann_software/recipesplayer/activites/RecipeDetailActivity.java
@@ -1,5 +1,7 @@
 package bingemann_software.recipesplayer.activites;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,5 +14,11 @@ public class RecipeDetailActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+    }
+
+    public static void start(Context context)
+    {
+        Intent intent = new Intent(context, RecipeDetailActivity.class);
+        context.startActivity(intent);
     }
 }
